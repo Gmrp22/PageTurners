a12f896c488d90c63718636429eb5ac86fc21628
gmrp22 <gmrp22@hotmail.com>
2021-10-25 20:49:36 -0600

    NavBar

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,10 @@
+import "./style.css";
+export default function NavBar() {
+  return (
+    <div className="navbar">
+      <a>Inicio</a>
+      <a> Libros </a>
+      <a>Biblioteca</a>
+    </div>
+  );
+}
