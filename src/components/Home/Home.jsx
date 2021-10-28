6cc6be4538fc89e9262e979c47002ed8fcd51cb8
gmrp22 <gmrp22@hotmail.com>
2021-10-28 16:50:04 -0600

    Creacion de galeria

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,6 @@
 import React, {Component} from "react";
 import Header from "./Header/Header";
 import Information from "./Information/Information";
-import './style.css'
 export default class Home extends Component {
 
     render(){
